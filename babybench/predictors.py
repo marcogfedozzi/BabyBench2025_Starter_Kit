<Add> log prdictor output for debugging <FIX> action pred scaling

babybench/predictors.py
@@ -8,7 +8,6 @@ from typing import List, Optional, Tuple
 from functools import partial
 from contextlib import nullcontext
 
-
 class ForwardInverseSurprisePredictor(TensorDictModule):
     """
     TensorDictModule wrapper around feat_extractor, forward and inverse predictors.
@@ -38,7 +37,8 @@ class ForwardInverseSurprisePredictor(TensorDictModule):
         eta: float = 1.0,
         dtype = torch.float32,
         detach_next_features: bool = False,
-        clamp_surprise: Optional[float] | Optional[Tuple[float, float]] = None
+        clamp_surprise: Optional[float] | Optional[Tuple[float, float]] = None,
+        log_net_outputs: bool = False,
     ):
         super().__init__(module=torch.nn.Identity(), in_keys=in_keys, 
                          out_keys=[("next", "reward"), "intrinsic_loss_fwd", "intrinsic_loss_inv"],
@@ -54,6 +54,7 @@ class ForwardInverseSurprisePredictor(TensorDictModule):
         self.inv_mod    = inverse_model.to(dtype)
 
         # loss/optim
+        # TODO: check if loss requires "target:" attribute
         self.fwd_loss_fn = forward_loss_fn
         self.inv_loss_fn = inverse_loss_fn
 
@@ -87,6 +88,9 @@ class ForwardInverseSurprisePredictor(TensorDictModule):
                 self._clamp_func = partial(torch.clamp, min=-clamp_surprise, max=clamp_surprise)
             else:
                 self._clamp_func = partial(torch.clamp, min=clamp_surprise[0], max=clamp_surprise[1])
+
+    
+        self._log_net_outputs = log_net_outputs
 
     def forward(self, td: TensorDict, loss_td: TensorDict) -> TensorDict:
         # gather observation parts and move to predictor device
@@ -125,8 +129,8 @@ class ForwardInverseSurprisePredictor(TensorDictModule):
         fwd_in = torch.cat([feats_t, actions], dim=-1)
         feats_t_next_pred = self.fwd_mod(fwd_in)
 
-        # map action_pred to action range (if needed)
-        action_pred = torch.sigmoid(action_pred) * (self._action_high - self._action_low) + self._action_low
+        # map action_pred to action range
+        action_pred = (F.normalize(action_pred, p=2, dim=-1, eps=1e-8)+1)/2 * (self._action_high - self._action_low) + self._action_low
 
         # cosine: normalize
         feats_t_next_n = F.normalize(feats_t_next, p=2, dim=-1, eps=1e-8)
@@ -142,9 +146,15 @@ class ForwardInverseSurprisePredictor(TensorDictModule):
         loss_td.set("loss_predictor", L_all)
         loss_td.set("loss_inv", L_inv.detach())
         loss_td.set("loss_fwd", L_fwd.detach())
+
+        if self._log_net_outputs:
+            td.set(("predictor","feats"),           F.normalize(feats_t.detach().cpu(), p=2, dim=-1, eps=1e-8))
+            td.set(("predictor","feats_next"),      feats_t_next_n.detach().cpu())
+            td.set(("predictor","feats_next_pred"), feats_t_next_pred_n.detach().cpu())
+            td.set(("predictor","action_pred"),     action_pred.detach().cpu())
         
-        # intrinsic reward (detach, move to CPU)
-        surprise = (self._eta / 2.0) * torch.linalg.vector_norm(feats_t_next_pred - feats_t_next, dim=-1)
+        # intrinsic reward (detach)
+        surprise = (self._eta / 2.0) * torch.linalg.vector_norm(feats_t_next_pred_n - feats_t_next_n, dim=-1)
         surprise = surprise.detach().unsqueeze(1)
         surprise = self._clamp_func(surprise)
 

babybench/rl_utils.py
@@ -24,13 +24,7 @@ from babybench import utils as bb_utils
 from torchrl.record.loggers import Logger, WandbLogger
 import os
 from torchrl.envs import default_info_dict_reader, EnvBase, EnvCreator
-import wandb
-
-
-
-def _check_key(cfg: DictConfig, key: str):
-	return key in cfg and getattr(cfg, key) is not None
-
+import tensordict
 
 class DuplicateFilter:
 	"""
@@ -62,7 +56,7 @@ def instantiate_transforms(transform_cfg: DictConfig) -> T.Compose:
 	:return: A list of instantiated transforms.
 	"""
 
-	logging.info(f"Instantiating Transforms: {transform_cfg.keys()}")
+	logging.debug(f"Instantiating Transforms: {transform_cfg.keys()}")
 
 	transforms: List[T.Transform] = []
 
@@ -77,7 +71,7 @@ def instantiate_transforms(transform_cfg: DictConfig) -> T.Compose:
 		for _, cb_conf in transform_cfg.items():
 			if isinstance(cb_conf, DictConfig) and "_target_" in cb_conf:
 
-				logging.info(f"Instantiating transform <{cb_conf._target_}>")
+				logging.debug(f"Instantiating transform <{cb_conf._target_}>")
 
 				"""Unnecessary right now
 				if "ObservationNorm" in cb_conf._target_ and (cb_conf.get("loc") is None or cb_conf.get("scale") is None):
@@ -98,7 +92,7 @@ def init_stats(env: TransformedEnv, num_iter: int = 1000):
 	
 	for trsf in env.transform:
 		if isinstance(trsf, T.ObservationNorm):
-			logging.info(f"Initializing transform {trsf} with {num_iter} iterations")
+			logging.debug(f"Initializing transform {trsf} with {num_iter} iterations")
 			trsf.init_stats(num_iter=num_iter)
 
 def register_script_resolvers():
@@ -109,6 +103,7 @@ def register_script_resolvers():
 	OmegaConf.register_new_resolver("get_device", lambda: torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 	OmegaConf.register_new_resolver("type",  lambda x: hydra.utils.get_object(x))
 	OmegaConf.register_new_resolver("cls",   lambda x: hydra.utils.get_class(x))
+	OmegaConf.register_new_resolver("tuple", lambda *args: tuple(args))
 
 
 def make_env(cfg: OmegaConf, bbench_config: Any, seed_mod: int = 0, is_eval: bool = False, reward_wrapper: gym.Wrapper = None) -> GymEnv:
@@ -139,7 +134,7 @@ def make_env(cfg: OmegaConf, bbench_config: Any, seed_mod: int = 0, is_eval: boo
 		env = GymWrapper(env)
 
 	if _info_keys:
-		logging.info(f"Setting info dict with keys {_info_keys}")
+		logging.debug(f"Setting info dict with keys {_info_keys}")
 		env.set_info_dict_reader(default_info_dict_reader(_info_keys))
 
 	_seed = cfg.eval.get("seed", -1) if is_eval else cfg.env.get("seed", -1)
@@ -488,12 +483,14 @@ def log_info_keys(cfg: DictConfig, td: TensorDict, logging_dict: Dict[str, Any])
 
 	info_keys = cfg.env.get("info_keys", [])
 
+	_flat_td = td.flatten_keys(separator='/')
+
 	if info_keys is None:
 		return
 
 	for key in info_keys:
-		if not key in td.keys():
+		if not key in _flat_td.keys():
 			# Do not issue warning or it will flood the terminal, simply ignore
 			continue
 		
-		logging_dict[f"info/{key}"] = td[key].mean().item()
+		logging_dict[f"info/{key}"] = _flat_td[key][0].cpu()